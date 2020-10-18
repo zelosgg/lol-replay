113b3b97a8a4a63a0eae688dc3aa02979fdc895d
Derek Chiang <me@derekchiang.com>
2020-10-18 01:57:56 -0700

    Set tracking

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ cd $GOPATH/src/github.com/1lann/lol-replay
 pwd
 ls
 git status
-git checkout master
-git status
 git remote add origin https://github.com/zelosgg/lol-replay.git
 git remote show
 git remote show origin
+git checkout master
+git status
+git branch --set-upstream-to=origin/master master
 go get -v -u ./server
 go build -o ./app ./server
