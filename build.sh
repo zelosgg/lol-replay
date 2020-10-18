b9448c883d51035e02ca967a246abc5e97453c7e
Derek Chiang <me@derekchiang.com>
2020-10-18 01:33:50 -0700

    Build on master

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 mkdir -p $GOPATH/src/github.com/1lann
 ln -s $GOPATH/src/github.com/zelosgg/lol-replay $GOPATH/src/github.com/1lann/lol-replay
 cd $GOPATH/src/github.com/1lann/lol-replay
+git checkout master
 go get -v -u ./...
 go build -o ./app ./server
