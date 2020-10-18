6d0e2fefa4dfcf413f4e65e5bd753636143ff60f
Derek Chiang <me@derekchiang.com>
2020-10-18 01:41:52 -0700

    Only go get the server

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir -p $GOPATH/src/github.com/1lann
 ln -s $GOPATH/src/github.com/zelosgg/lol-replay $GOPATH/src/github.com/1lann/lol-replay
 cd $GOPATH/src/github.com/1lann/lol-replay
 git checkout master
-go get -v -u ./...
+go get -v -u ./server
 go build -o ./app ./server
