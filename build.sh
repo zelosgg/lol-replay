14b940f752854b750ba61e8ac3a115991bb724ab
Derek Chiang <me@derekchiang.com>
2020-10-18 02:02:49 -0700

    Clean up build script and bind to 80

--- build.sh
+++ build.sh
@@ -2,14 +2,8 @@
 mkdir -p $GOPATH/src/github.com/1lann
 ln -s $GOPATH/src/github.com/zelosgg/lol-replay $GOPATH/src/github.com/1lann/lol-replay
 cd $GOPATH/src/github.com/1lann/lol-replay
-pwd
-ls
-git status
 git remote add origin https://github.com/zelosgg/lol-replay.git
-git remote show
-git remote show origin
 git checkout master
-git status
 git branch --set-upstream-to=origin/master master
 go get -v -u ./server
 go build -o ./app ./server
