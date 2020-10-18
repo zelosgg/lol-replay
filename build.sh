e34039e6e3de1f7e7a60971a7bf225eef4c9aa2d
Derek Chiang <me@derekchiang.com>
2020-10-18 01:31:25 -0700

    Create the correct go path

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mkdir /var/data/recordings
+mkdir -p $GOPATH/src/github.com/1lann
+ln -s $GOPATH/src/github.com/zelosgg/lol-replay $GOPATH/src/github.com/1lann/lol-replay
+cd $GOPATH/src/github.com/1lann/lol-replay
 go get -v -u ./...
 go build -o ./app ./server
-
